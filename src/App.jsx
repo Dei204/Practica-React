824675bf7569a869dc318e2bb61b4fd625e53aeb
Dei204 <deilerquesda08@gmail.com>
2024-09-02 13:09:34 -0600

    Cambios

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='contenerdorTop'>
+      <div className='Contenerdor'>
+        <p>Inicio</p>
+
+        <p>Registro</p>
+
+        <p>Contactos</p>
+
+        <p>Somos</p>
+
+        <p>Informacion</p>
+
+      </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
